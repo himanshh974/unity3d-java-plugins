fb34c767b2a030ca9a0f5b48d9c4254f143cd914
himanshh974 <60419105+himanshh974@users.noreply.github.com>
2020-06-01 11:19:21 +0530

    Add files via upload
    Unity Script for the toast plugin

---
+++ SendMsg.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SendMsg : MonoBehaviour
+{
+
+    public InputField TextInput;
+    public string text;
+
+    public void GetText()
+    {
+        text = TextInput.text;
+        MsgToPlugin(text);
+    }
+
+    public void MsgToPlugin(string msg)
+    {
+        Debug.Log(msg);
+
+        AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject bridge = new AndroidJavaObject("com.example.plugin.FirstPluginTest");
+        object[] parameters = new object[2];
+        parameters[0] = unityActivity;
+        parameters[1] = msg;
+        bridge.Call("PrintString", parameters);
+    }
+}
